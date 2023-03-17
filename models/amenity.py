fd647e62c179894ca0cbb9b0f1e8604311bae361
mirachuks <104357108+mirachuks@users.noreply.github.com>
2023-03-17 20:16:30 +0100

    Delete models directory

--- models/amenity.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" base model import """
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    """ Amenity class definition """
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" base_model import """
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    """ State class definition """
-    name = ""
-    
